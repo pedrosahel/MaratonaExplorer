c7c114b2bdd2315beba0e247009592ba85fb05e9
lordesaphe <49987286+lordesaphe@users.noreply.github.com>
2022-06-14 12:12:24 -0300

    Acabamentos finais!

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const answers = [
     "Você não está preparado para a resposta.",
     "Com certeza!",
     "Um espírito malígno não está me permitindo ver a resposta.",
+    "Definitivamente!",
+    "Você sabe que sim!",
     "Talvez uma massagem espiritual resolva seu problema.",
     "Você não está concentrado o suficiente na pergunta.",
     "Os espíritos falaram que sim.",
@@ -15,25 +17,56 @@ const answers = [
     "Isso está certo.",
     "Se você estava esperando uma resposta positiva, sinto muito!"
 ]
+const noQuestions = [
+    "Faça uma pergunta!",
+    "Você sabe que eu cobro por pergunta feita, não sabe?",
+    "Não posso responder sem saber a pergunta.",
+    "Sou um paranormal, não um telepata.",
+]
 
 const answerElement = document.querySelector("#answer")
 const questionElement = document.querySelector("#question")
+const questionButton = document.querySelector("#questioButton")
+
+let previousQuestion
 
 function questionMade() {
 
     answerElement.style.opacity = 1;
 
-    if(questionElement.value == "")
+    if(questionElement.value == previousQuestion)
     {
-        alert("Digite sua pergunta!")
-        return
+        resetQuestion();
+        questionButton.setAttribute('disabled');
+        return;
     }
 
-    const answersLenght = answers.length
-    const randomNumber = Math.floor(Math.random() * answersLenght)
-    answerElement.innerHTML = "<div>" + answers[randomNumber] + "</div"
+    previousQuestion = questionElement.value;
 
-    setTimeout(function() {
-        answerElement.style.opacity = 0;
+    if(questionElement.value != "")
+    {
+        answerDisplay(answers);   
+    }
+    else
+    {
+        answerDisplay(noQuestions);
+    }
+
+}
+
+function answerDisplay(myStringArray) {
+    const randomNumber = Math.floor(Math.random() * myStringArray.length);
+    answerElement.innerHTML = "<div>" + myStringArray[randomNumber] + "</div";
+
+    resetQuestion();
+}
+
+function resetQuestion() {
+    setTimeout(function(){
+        answerElement.style.opacity = 0; clearQuestion();
     }, 3000)
+}
+
+function clearQuestion() {
+    questionElement.value = "";
 }
